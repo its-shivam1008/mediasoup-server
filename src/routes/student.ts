691c9f2f1498277fdc2349912acb0d255bf39c48
its-shivam1008 <tanil5960@gmail.com>
2025-03-13 17:36:38 +0530

    add password hashing and password comparison

--- src/routes/student.ts
+++ src/routes/student.ts
@@ -1,6 +1,31 @@
-import express from "express";
+import express, { Request, Response, Router } from "express";
 import { prismaClient } from "../lib/db";
+import bcrypt from "bcryptjs";
+import { ApiResponse } from "../types/ApiResponse";
 const router = express.Router();
+
+const hashPassword = async (data:any)=>{
+    try{
+
+        const salt = await bcrypt.genSalt(8);
+
+        const hashedPasssword = await bcrypt.hash(data.password, salt)
+
+        data.password  = hashedPasssword;
+
+    }catch(err){
+        console.error("Error in hashing password",err);
+    }
+}
+
+const comparePasssword = async (candidatePassword:string, userPassword:string)=>{
+    try{
+        const isMatch = await bcrypt.compare(candidatePassword, userPassword);
+        return isMatch;
+    }catch(err){
+        console.log("Error in comparing passwords",err);
+    }
+}
 
 router.get('/', async(req, res) => {
     res.status(200).send("Hellow from users");
@@ -8,14 +33,37 @@ router.get('/', async(req, res) => {
 
 router.post('/signup', async(req, res) => {
     try{
-        const data1:any = req.body;
-        console.log(data1);
-        const response = await prismaClient.student.create({data:{...data1}});
+        const data:any = req.body;
+        await hashPassword(data);
+        const response = await prismaClient.student.create({data:{...data}});
         res.status(200).json({response});
     }catch(err){
         res.status(500).json({message:"Internal server error", err});
     }
 
 });
+
+
+router.post('/login', async(req:Request, res:Response) =>{
+    try{
+        const {email, password}:{email:string, password:string} = req.body;
+        if(!email || !password){
+            res.status(404).json({success:false, message:"Email and passssword is needed"});
+            return;
+        }
+        const response = await prismaClient.student.findUnique({where:{email:email}});
+        if(!response){
+            res.status(404).json({success:false, message:"Unable to find user"});
+            return;
+        }
+        if(!(await comparePasssword(password, response?.password as string))){
+            res.status(400).json({success:false, message:"Wrong password"});
+            return;
+        }
+        res.status(200).json({success:true, message:"Logged in success"});
+    }catch(err){
+        res.status(500).json({success:false, message:"Internal server error"});
+    }
+})
 
 export default router;
