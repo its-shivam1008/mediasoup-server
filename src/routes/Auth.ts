ddcbf762d5c248e801683f52ecd91039a6987e17
its-shivam1008 <tanil5960@gmail.com>
2025-04-02 00:11:57 +0530

    add some fixes

--- src/routes/Auth.ts
+++ src/routes/Auth.ts
@@ -74,7 +74,7 @@ router.post('/signup', async(req:Request, res:Response) => {
             id:response.id,
             email:response.email,
             role:response.role,
-            isVerified:response.isVerified
+            isVerified:false
         }
         const token = generateToken(payload);
 
@@ -113,7 +113,7 @@ router.post('/login', async(req:Request, res:Response) =>{
             id:response.id,
             email:response.email,
             role:response.role,
-            isVerified:response.isVerified
+            isVerified:true
         }
         const token = generateToken(payload);
         res.status(200).json({message:"User logged in", success:true, token});
